e2747b6364b627b69bacc1a4d6b1a38a00461626
Nahid Hasan <mnh.nahid35@gmail.com>
2021-08-26 20:38:46 +0600

    more endpoints added

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/nahidhasan98/todo/api"
 )
 
 func main() {
-	fmt.Println("Hello")
+	router := gin.Default()
+
+	router.POST("/api/login", func(ctx *gin.Context) {
+		api.Login(ctx)
+	})
+
+	// user group
+	user := router.Group("/api/user")
+	{
+		user.GET("", func(ctx *gin.Context) { api.GetUsers(ctx) })
+		user.GET("/:id", func(ctx *gin.Context) { api.GetUser(ctx) })
+	}
+
+	// todo group
+	todo := router.Group("/api/todo")
+	{
+		todo.GET("/:id", func(ctx *gin.Context) { api.GetTodo(ctx) })
+		todo.GET("", func(ctx *gin.Context) { api.GetTodos(ctx) })
+
+		todo.DELETE("/:id", func(ctx *gin.Context) { api.DeleteTodo(ctx) })
+		todo.DELETE("", func(ctx *gin.Context) { api.DeleteTodos(ctx) })
+
+		todo.POST("", func(ctx *gin.Context) { api.AddTodo(ctx) })
+		todo.PATCH("/:id", func(ctx *gin.Context) { api.UpdateTodo(ctx) })
+	}
+
+	router.Run(":8080")
 }
