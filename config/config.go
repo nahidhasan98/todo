a6589646cc30a94638d801945cbed35c9713c129
Nahid Hasan <mnh.nahid35@gmail.com>
2021-10-01 19:02:47 +0600

    Dockerfile added

--- config/config.go
+++ config/config.go
@@ -1,9 +1,11 @@
 package config
 
-const (
-	DbConnectionString = "localhost:27017"
+import "os"
 
-	DBName    = "TODO"
+var DbConnectionString = os.Getenv("MongoDB_host")
+
+const (
+	DBName    = "todo"
 	AuthTable = "user"
 	TodoTable = "todo"
 	UserTable = "user"
